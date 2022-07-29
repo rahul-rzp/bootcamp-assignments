a74aba8ec6c3b782fbf0a22ed0f3360337a6edbd
Rahul Makhija <rahul.makhija@razorpay.com>
2022-07-29 10:21:02 +0530

    add responsiveness on mobile add no content available UI

--- view.js
+++ view.js
@@ -1,29 +1,39 @@
-const localStorageData = localStorage.getItem('contact_book_data');
-const contactBookData = localStorageData ? JSON.parse(localStorageData) : [];
-console.log(contactBookData, "data");
+function displayContactSection(){
+  const localStorageData = localStorage.getItem('contact_book_data');
+  const contactBookData = localStorageData ? JSON.parse(localStorageData) : [];
+  console.log(contactBookData, "data");
 
-const tableElement = document.createElement('table');
-const tableHeader = document.createElement('thead');
-const tableHeaderName = document.createElement('th');
-tableHeaderName.innerHTML = "Name";
+  if(!contactBookData || contactBookData.length === 0){
+    console.log('no data');
+    document.querySelector('.content').innerHTML = "No content available";
+    return ;
+  }
+  
+  const tableElement = document.createElement('table');
+  const tableHeader = document.createElement('thead');
+  const tableHeaderName = document.createElement('th');
+  tableHeaderName.innerHTML = "Name";
+  
+  const tableHeaderContact = document.createElement('th');
+  tableHeaderContact.innerHTML = "Contact";
+  tableElement.appendChild(tableHeader);
+  tableHeader.appendChild(tableHeaderName);
+  tableHeader.appendChild(tableHeaderContact);
+  
+  contactBookData.forEach(item => {
+    const trElement = tableElement.insertRow();
+    const nameElement = document.createElement('td');
+    nameElement.innerHTML = item.name;
+    trElement.appendChild(nameElement);
+  
+    const contactElement = document.createElement('td');
+    contactElement.innerHTML = item.contact;
+    trElement.appendChild(contactElement);
+  
+    tableElement.appendChild(trElement);
+  });
+  
+  document.querySelector('.content').appendChild(tableElement);
+}
 
-const tableHeaderContact = document.createElement('th');
-tableHeaderContact.innerHTML = "Contact";
-tableElement.appendChild(tableHeader);
-tableHeader.appendChild(tableHeaderName);
-tableHeader.appendChild(tableHeaderContact);
-
-contactBookData.forEach(item => {
-  const trElement = tableElement.insertRow();
-  const nameElement = document.createElement('td');
-  nameElement.innerHTML = item.name;
-  trElement.appendChild(nameElement);
-
-  const contactElement = document.createElement('td');
-  contactElement.innerHTML = item.contact;
-  trElement.appendChild(contactElement);
-
-  tableElement.appendChild(trElement);
-});
-
-document.querySelector('.content').appendChild(tableElement);
+displayContactSection();
